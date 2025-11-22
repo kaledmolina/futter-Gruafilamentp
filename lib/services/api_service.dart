3718bc78f81df0977c516b11477969d6135b3f9b
MoisesCorcho <mcorchoperez@gmail.com>
2025-11-21 19:18:40 -0500

    feat: Update Firebase configuration and API endpoints for new project setup

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -6,8 +6,8 @@ import '../models/orden_model.dart';
 import '../models/user_model.dart';
 
 class ApiService {
-  static const String baseUrl = 'https://gruap.kaledcloud.tech/api';
-  static const String domain = 'https://gruap.kaledcloud.tech';
+  static const String baseUrl = 'https://grua2.moisescorchodev.com/api';
+  static const String domain = 'https://grua2.moisescorchodev.com';
 
   String getBaseUrl() => baseUrl;
 
